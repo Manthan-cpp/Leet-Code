7fbdb94870a63fc785ec39b57203fd98de36ebe0
Manthan Jaiswal <manthanjswl@gmail.com>
2026-07-02 21:10:00 +0530

    Time: 255 ms (11.66%), Space: 12.7 MB (38.38%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        vector<bool> prime(n+1,true);
+        int c=0,j=2;
+        for(int i=2;i<n;i++){
+            if(prime[i]){
+                c++;
+                for(j=2;j*i<n;j++){
+                    prime[j*i]=0;
+                }
+            }
+        }
+        return c;
+    }
+};
