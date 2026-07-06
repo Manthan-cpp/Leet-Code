0e08acc446bbb5cd41550a99a87c7074f84a99a4
Manthan Jaiswal <manthanjswl@gmail.com>
2026-07-06 20:11:54 +0530

    Time: 0 ms (100%), Space: 26.1 MB (57.59%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,37 +1,29 @@
-#include <algorithm>
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int n=height.size(),i=0,max1=0,max2=0;
-        int j=n-1;
-        vector<int> r; 
-        vector<int> l;
-        vector<int> fin;
-        if(height.empty()){
-            return 0;
+        int n = height.size();
+        if (n == 0) return 0;
+
+        int i = 0, j = n - 1;
+        int max1 = 0, max2 = 0;
+        int sum = 0;
+
+        while (i < j) {
+            if (height[i] <= height[j]) {
+                if (height[i] >= max1)
+                    max1 = height[i];
+                else
+                    sum += max1 - height[i];
+                i++;
+            } else {
+                if (height[j] >= max2)
+                    max2 = height[j];
+                else
+                    sum += max2 - height[j];
+                j--;
+            }
         }
-        max1=height[0];
-        while(i<n){
-            max1=max(max1,height[i]);
-            l.push_back(max1);
-            i++;
-        } 
-        max2=height[n-1];
-        while(j>=0){
-            max2=max(max2,height[j]);
-            r.push_back(max2);
-            j--;
-        }
-        reverse(r.begin(),r.end());
-        i=0;
-        while(i<n){
-            fin.push_back(min(l[i],r[i])-height[i]);
-            i++;
-        }
-        int sum=0;
-        for(int i=0;i<fin.size();i++){
-            sum+=fin[i];
-        }
+
         return sum;
     }
 };
