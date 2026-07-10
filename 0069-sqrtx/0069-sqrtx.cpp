35dcfc3ab41a846354c67bcf85d61061faf849e3
Manthan Jaiswal <manthanjswl@gmail.com>
2026-07-10 13:27:40 +0530

    Time: 11 ms (12.31%), Space: 8.7 MB (14.09%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int i;
+        if (x==1||x==0){
+            return x;
+        }
+       
+            for (i = 0; (long long)i*i <= x ; i++) {
+                
+            }
+    return i-1;
+    }
+};
