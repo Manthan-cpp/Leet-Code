4d8a5cca1879017845cd2158c5a89fba800e9661
Manthan Jaiswal <manthanjswl@gmail.com>
2026-07-20 15:55:56 +0530

    Time: 0 ms (100%), Space: 18.2 MB (66.35%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.cpp
+++ 1260-shift-2d-grid/1260-shift-2d-grid.cpp
@@ -1,32 +1,20 @@
 class Solution {
 public:
     vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
-        int m = grid.size(), n= grid[0].size();
-        vector<vector<int>> ans;
-        int arr[m*n];
-        if(k%(m*n)==0){
+        int m = grid.size(), n = grid[0].size(), total = m * n;
+        vector<vector<int>> ans(m, vector<int>(n));
+        k %= total;
+        if (k % (total) == 0) {
             return grid;
         }
-        else{
-            k=k%(m*n);
-        }
-        int ind=0;
-        for(int i = 0;i<m;i++)
-        {
-            for(int j =0 ;j<n;j++){
-                arr[ind++]=grid[i][j];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int oldind = i * n + j;
+                int newind = (oldind + k) % total;
+                int newrow = newind / n;
+                int newcol = newind % n;
+                ans[newrow][newcol]=grid[i][j];
             }
-        }
-        ind = 0;
-        for(int i = 0;i<m;i++){
-            vector<int> temprow;
-            for(int j = 0 ; j < n ; j++){
-                int idx= (ind-k+(m*n))%(m*n);
-                int val = arr[idx];
-                ind++;
-                temprow.push_back(val);
-            }
-            ans.push_back(temprow);
         }
         return ans;
     }
