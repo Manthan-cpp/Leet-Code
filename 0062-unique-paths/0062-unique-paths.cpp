c2e605c7974a177284f0db80a5391920c514441d
Manthan Jaiswal <manthanjswl@gmail.com>
2026-07-22 16:12:33 +0530

    Time: 0 ms (100%), Space: 7.7 MB (97.77%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        long long ans = 1;
-        int N = m + n - 2;
-        int R = min(m - 1, n - 1);
-
-        for (int i = 1; i <= R; i++) {
-            ans = ans * (N - R + i) / i;
+        int board[m][n];
+        for(int i = 0;i<n;i++){
+            board[0][i]=1;
         }
-
-        return (int)ans;
+        for(int i=0;i<m;i++){
+            board[i][0]=1;
+        }
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                board[i][j]=board[i-1][j] + board[i][j-1];
+            }
+        }
+        return board[m-1][n-1];
     }
 };
