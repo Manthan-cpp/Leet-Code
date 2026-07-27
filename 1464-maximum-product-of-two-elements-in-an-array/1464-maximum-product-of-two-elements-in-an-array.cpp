ff43683d6371d0b3cad4efdb0074c59767b44b2e
Manthan Jaiswal <manthanjswl@gmail.com>
2026-07-27 18:23:11 +0530

    Time: 0 ms (100%), Space: 13.5 MB (25.65%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.cpp
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.cpp
@@ -3,9 +3,9 @@ public:
     int maxProduct(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         int n=nums.size();
-        if(n==1){
-            return nums[0];
-        }
+        // if(n==1){
+        //     return nums[0];
+        // }
         return (nums[n-1]-1)*(nums[n-2]-1);
     }
 };
