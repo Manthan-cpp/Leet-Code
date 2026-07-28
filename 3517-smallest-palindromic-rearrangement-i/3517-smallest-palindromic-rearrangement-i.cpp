f16704f89e91ad955c52c1ffbe78256b87333f3f
Manthan Jaiswal <manthanjswl@gmail.com>
2026-07-28 19:03:06 +0530

    Time: 78 ms (41.32%), Space: 54.1 MB (95.38%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
     string smallestPalindrome(string s) {
-        int n = s.length();
+        int n = s.length(),j=0;
         if(n==1||n==3){
             return s;
         }
         sort(s.begin(),s.begin()+n/2);
-        sort(s.begin()+(n+1)/2,s.end(), greater<char>());
+        j=n-1;
+        int i=0;
+        while(j>=(n+1)/2){
+            s[j]=s[i];
+            i++;
+            j--;
+        }
         return s;
     }
 };
