cbef4c557b6570aa9040ff89e54b01032563d555
Manthan Jaiswal <manthanjswl@gmail.com>
2026-07-29 19:35:20 +0530

    Time: 0 ms (100%), Space: 10.7 MB (71.39%) - LeetHub

--- 0273-integer-to-english-words/0273-integer-to-english-words.cpp
+++ 0273-integer-to-english-words/0273-integer-to-english-words.cpp
@@ -1,49 +1,46 @@
 class Solution {
+    vector<string> belowTen = {
+        "", "One", "Two", "Three", "Four",
+        "Five", "Six", "Seven", "Eight", "Nine"
+    };
+
+    vector<string> belowTwenty = {
+        "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen",
+        "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"
+    };
+
+    vector<string> belowHundred = {
+        "", "Ten", "Twenty", "Thirty", "Forty",
+        "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"
+    };
+
 public:
-    // Unordered maps to store words for numbers less than 10, 20, and 100
-
-    unordered_map<int, string> belowTen = {
-        {0, ""},     {1, "One"}, {2, "Two"},   {3, "Three"}, {4, "Four"},
-        {5, "Five"}, {6, "Six"}, {7, "Seven"}, {8, "Eight"}, {9, "Nine"}};
-
-    unordered_map<int, string> belowTwenty = {
-        {10, "Ten"},      {11, "Eleven"},  {12, "Twelve"},  {13, "Thirteen"},
-        {14, "Fourteen"}, {15, "Fifteen"}, {16, "Sixteen"}, {17, "Seventeen"},
-        {18, "Eighteen"}, {19, "Nineteen"}};
-
-    unordered_map<int, string> belowHundred = {
-        {1, "Ten"},   {2, "Twenty"},  {3, "Thirty"}, {4, "Forty"}, {5, "Fifty"},
-        {6, "Sixty"}, {7, "Seventy"}, {8, "Eighty"}, {9, "Ninety"}};
-    string solve(int num) {
-        if (num < 10) {
-            return belowTen[num];
-        }
-        if (num < 20) {
-            return belowTwenty[num];
-        }
-        if (num < 100) {
-            return belowHundred[num/10] +
-                   (num % 10 != 0 ? " " + belowTen[num%10] : "");
-        }
-        if (num < 1000) {
-            return solve(num / 100) + " Hundred" +
-                   (num % 100 != 0 ? " " + solve(num % 100) : "");
-        }
-        if (num < 1000000) {
-            return solve(num / 1000) + " Thousand" +
-                   (num % 1000 != 0 ? " " + solve(num % 1000) : "");
-        }
-        if (num < 1000000000) {
-            return solve(num / 1000000) + " Million" +
-                   (num % 1000000 != 0 ? " " + solve(num % 1000000) : "");
-        }
-        return solve(num / 1000000000) + " Billion" +
-               (num % 1000000000 != 0 ? " " + solve(num % 1000000000) : "");
-    }
     string numberToWords(int num) {
-        if (num == 0) {
-            return "Zero";
-        }
-        return solve(num);
+        if (num == 0) return "Zero";
+
+        if (num < 10)
+            return belowTen[num];
+
+        if (num < 20)
+            return belowTwenty[num - 10];
+
+        if (num < 100)
+            return belowHundred[num / 10] +
+                   (num % 10 ? " " + belowTen[num % 10] : "");
+
+        if (num < 1000)
+            return numberToWords(num / 100) + " Hundred" +
+                   (num % 100 ? " " + numberToWords(num % 100) : "");
+
+        if (num < 1000000)
+            return numberToWords(num / 1000) + " Thousand" +
+                   (num % 1000 ? " " + numberToWords(num % 1000) : "");
+
+        if (num < 1000000000)
+            return numberToWords(num / 1000000) + " Million" +
+                   (num % 1000000 ? " " + numberToWords(num % 1000000) : "");
+
+        return numberToWords(num / 1000000000) + " Billion" +
+               (num % 1000000000 ? " " + numberToWords(num % 1000000000) : "");
     }
 };
