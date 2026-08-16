293e9d7e68bbe800d2e7bf4a2a782d427008d9fa
Manthan Jaiswal <manthanjswl@gmail.com>
2026-08-16 18:48:04 +0530

    Time: 2 ms (80.71%), Space: 131.2 MB (92.89%) - LeetHub

---
+++ 2029-stone-game-ix/2029-stone-game-ix.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool stoneGameIX(vector<int>& stones) {
+        int count[3] = {0};
+
+        for (int stone : stones) {
+            count[stone % 3]++;
+        }
+
+        // Even number of stones divisible by 3
+        if (count[0] % 2 == 0) {
+            return min(count[1], count[2]) > 0;
+        }
+
+        // Odd number of stones divisible by 3
+        return abs(count[1] - count[2]) > 2;
+    }
+};
