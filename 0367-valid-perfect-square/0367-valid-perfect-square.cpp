ce846329c2c42ee41fb87519c9a665b938488ca4
Manthan Jaiswal <manthanjswl@gmail.com>
2026-08-23 20:40:05 +0530

    Time: 0 ms (100%), Space: 7.7 MB (53.54%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        long long l = 1, r = num;
+        while (l <= r) {
+            long long m = l + (r - l) / 2;
+            if (m * m == num) return true;
+            if (m * m < num) l = m + 1;
+            else r = m - 1;
+        }
+        return false;
+    }
+};
