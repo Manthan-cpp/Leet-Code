c5e0ae398499f7c17e6baa18a4655a6c45a2d0f1
Manthan Jaiswal <manthanjswl@gmail.com>
2026-08-30 15:33:42 +0530

    Time: 0 ms (100%), Space: 9.5 MB (17.74%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        int n=s.length();
+        unordered_map<char,int> mp;
+        for(char &ch:s){
+            mp[ch]++;
+        }
+        int result=0;
+        bool odd=false;
+        for(auto &it:mp){
+            if(it.second%2==0){
+                result+=it.second;
+            }
+            else{
+                result+=it.second-1;
+                odd=true;
+            }
+        }
+        if(odd){
+            result++;
+        }
+        return result;
+    }
+};
